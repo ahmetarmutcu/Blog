d4275846abdf71d880631fdd8284ba7c7c590d68
ahmetarmutcu <ahmetarmutcu2017@gmail.com>
2024-01-21 12:30:20 +0300

    Kategori ekleme işleminde ajax yöntemi kullanıldı.

---
+++ Blog.Web/wwwroot/js/categoryAdd.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    $("#btnSave").click(function (event) {
+        event.preventDefault();
+        var addUrl = app.Urls.categoryAddUrl;
+        var redirectUrl = app.Urls.articleAddUrl;
+
+        var categoryAddDto = {
+            Name: $("input[id=categoryName]").val()
+        }
+        var jsonData = JSON.stringify(categoryAddDto);
+
+        $.ajax({
+            url: addUrl,
+            type: "POST",
+            contentType: "application/json; charset=utf-8",
+            datatype:"json",
+            data: jsonData,
+            success: function (data) {
+                setTimeout(function () {
+                    window.location.href = redirectUrl;
+                },1500);
+            },
+            error: function () {
+                toastr.error("Bir hata oluştu", "Hata");
+            }
+        });
+    });
+});
