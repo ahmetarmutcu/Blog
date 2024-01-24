bad0a456f74d7972570eec8fc860b8d50cb91a7b
ahmetarmutcu <ahmetarmutcu2017@gmail.com>
2024-01-24 21:58:08 +0300

    Rolü ile birlikte kullanıcı ekleme işlemi yapıldı.

--- Blog.Entity/Entities/AppUser.cs
+++ Blog.Entity/Entities/AppUser.cs
@@ -6,7 +6,7 @@ namespace Blog.Entity.Entities
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public Guid ImageId { get; set; }
+        public Guid ImageId { get; set; }=Guid.Parse("7C8DCD23-143F-4145-AC5D-3D2172E8B876");
         public Image Image { get; set; }
         public ICollection<Article> Articles { get; set; }
     }
