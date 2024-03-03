359de93a9f20c27054b2c32caf6dd9b648aa053b
ahmetarmutcu <ahmetarmutcu2017@gmail.com>
2024-03-03 12:54:28 +0300

    Admin index sayfasında analiz ve grafik üzerinde veri gösterme

--- Blog.Data/Repositories/Abstractions/IRepository.cs
+++ Blog.Data/Repositories/Abstractions/IRepository.cs
@@ -17,9 +17,6 @@ namespace Blog.Data.Repositories.Abstractions
         Task<T> UpdateAsync(T entity);
         Task DeleteAsync(T entity);
         Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
-
-
         Task<int> CountAsync(Expression<Func<T, bool>> predicate = null);
-
     }
 }
--- Blog.Web/Areas/Admin/Controllers/HomeController.cs
+++ Blog.Web/Areas/Admin/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using Blog.Entity.Entities;
 using Blog.Service.Services.Abstractions;
+using Blog.Service.Services.Concrete;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Security.Claims;
 
 namespace Blog.Web.Areas.Admin.Controllers
@@ -12,18 +14,36 @@ namespace Blog.Web.Areas.Admin.Controllers
     public class HomeController : Controller
     {
         private readonly IArticleService articleService;
+        private readonly IDashboardService dashbordService;
         private readonly UserManager<AppUser> userManager;
 
-        public HomeController(IArticleService articleService,UserManager<AppUser> userManager)
+        public HomeController(IArticleService articleService,IDashboardService dashbordService)
         {
             this.articleService = articleService;
-            this.userManager = userManager;
+            this.dashbordService = dashbordService;
         }
         public async Task<IActionResult> Index()
         {
             var articles = await articleService.GetAllArticlesWithCategoryNonDeleteAsync();
-            var loggedInUser = await userManager.GetUserAsync(HttpContext.User);
             return View(articles);
+        }
+        [HttpGet]
+        public async Task<IActionResult> YearlyArticleCounts()
+        {
+            var count = await dashbordService.GetYearlyArticleCounts();
+            return Json(JsonConvert.SerializeObject(count));
+        }
+        [HttpGet]
+        public async Task<IActionResult> TotalArticleCount()
+        {
+            var count = await dashbordService.GetTotalArticleCount();
+            return Json(count);
+        }
+        [HttpGet]
+        public async Task<IActionResult> TotalCategoryCount()
+        {
+            var count = await dashbordService.GetTotalCategoryCount();
+            return Json(count);
         }
 
     }
